1f4c19f4355ec3384c3e5c6e421adb96c5749a5a
Nhomar Hernandez <nhomar@gmail.com>
2015-03-24 00:03:16 -0600

    [PEP8] erp_vauxoo_com, l10n_mx_facturae_vauxoo_report, sale_order_report: Fixes descriptors pep8 and moved descritions to README.

--- erp_vauxoo_com/__openerp__.py
+++ erp_vauxoo_com/__openerp__.py
@@ -1,32 +1,8 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Odoo (Formely OpenERP), Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    Copyleft (Cl) 2008-2021 Vauxoo, C.A. (<http://vauxoo.com>)
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-{"name": "Odoo Vauxoo Instance",
+{
+    "name": "Odoo Vauxoo ERP Instance",
     "author": "Vauxoo",
     "summary": "All the necesary modules to auto install our instance.",
-    "description" : """
-Install all apps needed to comply with Vauxoo instance
-======================================================
-
-TODO: Document all the references (read www_vauxoo_com module as an example)
-                    """,
     "website": "http://www.vauxoo.com",
     "category": "Vauxoo",
     "version": "2.0",
@@ -81,9 +57,8 @@ TODO: Document all the references (read www_vauxoo_com module as an example)
     "data": [],
     "demo": [],
     "test": [
-        # Only our tests (the custom ones)
     ],
     "auto_install": False,
     "application": True,
     "installable": True,
- }
+}
---
+++ sale_order_report/README.md
@@ -0,0 +1,4 @@
+Custom Vauxoo Report.
+---
+
+TODO, describe here special features and some screens can help to know what it does.
