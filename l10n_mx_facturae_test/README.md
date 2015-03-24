ef0983ecb3034bedb582de71b37b2bddd132d1b3
Nhomar Hernandez <nhomar@gmail.com>
2015-03-23 23:45:40 -0600

    [PEP8] l10n_mx_facturae_test: now description moved to README file

---
+++ l10n_mx_facturae_test/README.md
@@ -0,0 +1,5 @@
+Electronic Invoice Test CFDI
+---
+
+This wizard execute test yaml of electronic invoice CFDI "N" times.
+This with finally of make test to the server.
--- l10n_mx_facturae_test/__openerp__.py
+++ l10n_mx_facturae_test/__openerp__.py
@@ -1,28 +1,4 @@
 # -*- encoding: utf-8 -*-
-#
-#    Module Writen to OpenERP, Open Source Management Solution
-#
-#    Copyright (c) 2013 Vauxoo - http://www.vauxoo.com/
-#    All Rights Reserved.
-#    info Vauxoo (info@vauxoo.com)
-#
-#    Coded by: Jorge Angel Naranjo (jorge_nr@vauxoo.com)
-#
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
 {
     "name": "Electronic Invoice Test CFDI",
     "version": "1.0",
@@ -35,14 +11,7 @@
         "l10n_mx_cfdi_test",
     ],
     "author": "Vauxoo",
-    "description": """
-Electronic Invoice Test CFDI
-==================
-
-This wizard execute test yaml of electronic invoice CFDI "N" times. 
-This with finally of make test to the server.
-    """,
-    "website": "http://vauxoo.com",
+    "website": "https://vauxoo.com",
     "category": "Addons Vauxoo",
     "data": [
         'wizard/facturae_test_wizard.xml',
