90a9fc656a93506a3032479817a6d20f882c7497
Nhomar Hernandez <nhomar@vauxoo.com>
2015-07-17 19:04:36 +0000

    [IMP] Doc and testing.

--- vauxoo_sale_reports/model/rst2html/html.py
+++ vauxoo_sale_reports/model/rst2html/html.py
@@ -23,29 +23,30 @@ TEMPLATE = '''
 %(body)s
 '''
 
+
 def rst2html(rst, opts=None):
+    '''Simple tool to convert rst to html to be embeded in any document
+
+    Example:
+
+        >>> chain = "Text in rst"
+        >>> rst2html(chain)
+        u'<div class="document">\\n<p>Text in rst</p>\\n</div>\\n'
+
+    @param: rst Text multiline in rst format
+    '''
     template = NamedTemporaryFile('w', suffix='.txt', delete=False)
     rst_opts = default_rst_opts.copy()
     if opts:
         rst_opts.update(opts)
-    # TODO: this should be a parameter
     template.write(TEMPLATE)
     template.close()
     rst_opts['template'] = template.name
-    out = publish_parts(rst, writer_name='html', settings_overrides=rst_opts)['html_body']
+    out = publish_parts(rst,
+                        writer_name='html',
+                        settings_overrides=rst_opts)['html_body']
     return out
 
 if __name__ == '__main__':
-    print rst2html(
-"""
-Title
------
-
-Hola Mundo.
-
-Subtitle
-========
-
-- Hola
-- Mundo
-""")
+    import doctest
+    doctest.testmod()
