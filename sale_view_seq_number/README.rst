7f040015274115f2cfba187a91efeb3ff295b709
Luis Llave <luisll@vauxoo.com>
2015-12-21 17:43:38 +0000

    [FIX] instance: Make visible sequence field in order line form

---
+++ sale_view_seq_number/README.rst
@@ -0,0 +1,5 @@
+View Sequence Number in Order Line
+==================================
+
+This module put sequence number field in show, inside
+order line form.
---
+++ sale_view_seq_number/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2015 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: luisll@vauxoo.com
+#    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
+############################################################################
