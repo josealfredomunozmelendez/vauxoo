eddb39fa6bda24d1d9bb8e17af6cd07cd9e3e5d3
Nhomar Hernandez <nhomar@vauxoo.com>
2017-03-01 02:20:08 -0600

    [IMP] vauxoo: Display name for taxes improved.
    As we defined that an HU is a task with subtaskis
    in the context of a sales order related with the task
    In order to have a visual help we are computing
    properly the display name in order to help with it.
    
    Closes vauxoo/instance#411

--- vauxoo/models/project.py
+++ vauxoo/models/project.py
@@ -1,5 +1,19 @@
 # coding: utf-8
-from odoo import models, fields
+from odoo import models, fields, api
+
+
+class Project(models.Model):
+
+    _inherit = "project.project"
+
+    label_acronymous = fields.Char(
+        copy="False", default="HU", size=4,
+        help="Acronymous to show as prefix in the display name for the tasks"
+             " without parent Tasks")
+    label_acronymous_subtasks = fields.Char(
+        copy="False", default="CA", size=4,
+        help="Acronymous to show as prefix in the display name for the tasks"
+             " with parent Tasks")
 
 
 class ProjectTask(models.Model):
@@ -7,6 +21,40 @@ class ProjectTask(models.Model):
     _inherit = "project.task"
 
     ticket_id = fields.Many2one(
-        "helpdesk.ticket",
-        string="Helpdesk Ticket",
-        copy="False")
+        "helpdesk.ticket", copy="False")
+    display_name = fields.Char(
+        "Name", compute="_compute_display_name", store=True, index=True)
+
+    @api.model
+    def default_get(self, field_list):
+        res = super(ProjectTask, self).default_get(field_list)
+        res.update({'name': ''})
+        return res
+
+    @api.depends('parent_id', 'sale_line_id', 'name')
+    def _compute_display_name(self):
+        # all user don't have access to sale orders
+        # check access and use superuser
+        self.check_access_rights("read")
+        self.check_access_rule("read")
+
+        for task in self.sudo():
+            if not task.name:
+                task.display_name = ''
+                continue
+            name = task.name
+            order = False
+            if task.sale_line_id:
+                order = task.sale_line_id.order_id.name
+            if not task.parent_id:
+                prefix = task.project_id.label_acronymous or ''
+                story = prefix + (str(task.id) or '...')
+                criteria = ''
+            else:
+                prefix = task.project_id.label_acronymous or ''
+                story = prefix + (str(task.parent_id.id))
+                prefix = task.project_id.label_acronymous_subtasks or ''
+                criteria = prefix + (str(task.id) or '...')
+            elements = [order, story, criteria, name]
+            elements = [e for e in elements if e]
+            task.display_name = ":".join(elements)
