1c197fe3f82c9a2746db787bfd692bd96ab4f78c
JulioSerna <julio@vauxoo.com>
2017-04-21 17:48:03 +0000

    [IMP] vauxoo: Invoiceable hours as delivered hours on sales order.

--- vauxoo/models/sale.py
+++ vauxoo/models/sale.py
@@ -14,6 +14,30 @@ class SaleOrderLine(models.Model):
         for line in self:
             line.desc2html = rst2html.html.rst2html(line.name)
 
+    @api.multi
+    def _compute_analytic(self, domain=None):
+        lines = {}
+        res = super(SaleOrderLine, self)._compute_analytic(domain)
+        if not domain:
+            domain = [('so_line', 'in', self.ids), ('amount', '<=', 0.0)]
+        data = self.env['account.analytic.line'].search(domain)
+        for line in self.env.context.get("force_so_lines", []):
+            lines.setdefault(line, 0.0)
+        for line_analytic in data:
+            if not line_analytic['product_uom_id']:
+                continue
+            line = line_analytic['so_line']
+            lines.setdefault(line, 0.0)
+            uom = line_analytic['product_uom_id']
+            qty = line_analytic.invoiceables_hours
+            if line.product_uom.category_id == uom.category_id:
+                qty = uom._compute_quantity(line_analytic.invoiceables_hours,
+                                            line.product_uom)
+            lines[line] += qty
+        for line, qty in lines.items():
+            line.qty_delivered = qty
+        return res
+
 
 class SaleOrder(models.Model):
 
