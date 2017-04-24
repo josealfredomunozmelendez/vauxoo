7ff3aa25f7b3ce709144269bc60d8817ef9ee2bc
oalca <oscar@vauxoo.com>
2017-04-24 10:45:05 -0500

    [IMP] theme_vauxoo: the menu is now sticky.

--- theme_vauxoo/static/src/js/menu.js
+++ theme_vauxoo/static/src/js/menu.js
@@ -9,12 +9,31 @@ odoo.define('theme_vauxoo.menu', function (require) {
     $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
         $(this).toggleClass('open');
     });
+
+    // make al in page links scroll smoothly
     $('a.smoothly').click(function(){
         $('html, body').animate({
             scrollTop: $( $(this).attr('href') ).offset().top
         }, 500);
         return false;
     });
+
     // enable popovers
     $('[data-toggle="popover"]').popover();
+
+    // make the menu sticky change color if background is transparent.
+    var connected = $("#oe_main_menu_navbar");
+    // if the user is logged then margin-top increases due sticky bar of the
+    // website editor
+    if(connected.length){
+        $(".sticky-header").addClass('mt46');
+    }
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= 100)  {
+            $('.sticky-header').addClass('black_header').removeClass('hidden-header');
+        }
+        else {
+            $('.sticky-header').addClass('hidden-header');
+        }
+    });
 });
