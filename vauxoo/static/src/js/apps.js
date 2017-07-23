4288acecd2820df98f07ebadbbeb9e770cbc08f8
Nhomar Hernandez <nhomar@vauxoo.com>
2017-07-23 17:28:56 -0500

    [IMP] Product converted to a widget

--- vauxoo/static/src/js/apps.js
+++ vauxoo/static/src/js/apps.js
@@ -1,8 +1,33 @@
 odoo.define('theme_vauxoo.apps', function (require) {
     "use strict";
-    var base = require('web_editor.base');
+    var ajax = require('web.ajax');
     var core = require('web.core');
+    var website = require('website.website');
+    var Widget = require('web.Widget');
     if(!$('.app-check').length) {
-        return $.Deferred().reject("DOM doesn't contain a app buton check");
+        return $.Deferred().reject("DOM doesn't contain a app button check");
     }
+    var App = Widget.extend({
+        events: {
+            'click .app-check' : 'onClick',
+        },
+        init: function(parent){
+            this._super(parent);
+        },
+        start: function(){
+            console.log('Started My App');
+
+            return this._super.apply(this, arguments);
+        },
+        onClick: function(){
+            var input = this.$('.app-check');
+            console.log(input.data());
+            console.log('Hello Widget clicked');
+        }
+    });
+    $('#on_premise_apps').ready(function() {
+        // TODO : make it work. For now, once the iframe is loaded, the value of #page_count is
+        // still now set (the pdf is still loading)
+        new App($(this)).setElement($('.app'));
+    });
 });
