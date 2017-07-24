6e691734392a6e294f8a9a98ec058649d6952592
Nhomar Hernandez <nhomar@vauxoo.com>
2017-07-24 01:27:12 -0500

    squash with minimal app

--- vauxoo/static/src/js/apps.js
+++ vauxoo/static/src/js/apps.js
@@ -7,9 +7,28 @@ odoo.define('theme_vauxoo.apps', function (require) {
     if(!$('.app-check').length) {
         return $.Deferred().reject("DOM doesn't contain a app button check");
     }
+    /**
+    Rendered on server side but widget generated in client side.
+    */
     var App = Widget.extend({
         events: {
             'click .app-check' : 'onClick',
+        },
+        init: function(parent){
+            this._super(parent);
+        },
+        start: function(){
+            return this._super.apply(this, arguments);
+        },
+        onClick: function(){
+            var input = this.$('.app-check');
+            console.log(input.data());
+            console.log('Hello Widget clicked');
+        }
+    });
+    var Order = Widget.extend({
+        events: {
+            'click #quick-or-premise' : 'onClick',
         },
         init: function(parent){
             this._super(parent);
@@ -25,9 +44,17 @@ odoo.define('theme_vauxoo.apps', function (require) {
             console.log('Hello Widget clicked');
         }
     });
+
+    var button_list = [];
+    $('.app').each(function( index ) {
+        var button = new App();
+        button.setElement($(this)).start();
+        button_list.push(button);
+    });
     $('#on_premise_apps').ready(function() {
         // TODO : make it work. For now, once the iframe is loaded, the value of #page_count is
         // still now set (the pdf is still loading)
-        new App($(this)).setElement($('.app'));
+//        new App($(this)).setElement($('.app'));
+        new Order($(this)).setElement($('.apps-tray'));
     });
 });
