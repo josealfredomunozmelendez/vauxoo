bed9a34bc24bbd3d98532b6194f0a4c4144041d0
JesusZapata <jesus@vauxoo.com>
2017-11-03 20:51:41 +0000

    [ADD] website_blog_rss: Create the new module website_blog_rss from addons-vauxo#8.0 change the old api

---
+++ website_blog_rss/models/blog_post.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+import time
+import datetime
+from email import utils
+from openerp import models, fields, api
+
+
+class BlogPost(models.Model):
+    _inherit = 'blog.post'
+
+    @api.multi
+    def _get_date(self):
+        posts = self
+        for post in posts:
+            date_obj = time.strptime(post.write_date, "%Y-%m-%d %H:%M:%S")
+            dt = datetime.datetime.fromtimestamp(time.mktime(date_obj))
+            write_tuple = dt.timetuple()
+            timestamp = time.mktime(write_tuple)
+            post.date_rfc2822 = utils.formatdate(timestamp)
+
+    date_rfc2822 = fields.Char(
+        compute=_get_date,
+        string="Date RFC-2822")
