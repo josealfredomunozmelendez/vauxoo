7e4fa182d2a0fba4328dbc5d9dca558759822d5f
Katherine Zaoral <kathy@vauxoo.com>
2017-11-20 02:48:32 +0000

    [ADD] vxmigration: use requirement file
    - add new file requirements.txt
    - migration.sh will let us to install requirements instead of explicitly
      indicate each dependency.
    - abastract install_requires reads directly from requirements.txt, this way
      the requirements are in one place only.

--- tools/setup.py
+++ tools/setup.py
@@ -2,18 +2,16 @@
 # -*- coding: utf-8 *-
 from setuptools import setup
 
+def read_requirements():
+    with open('requirements.txt', 'r') as reqfile:
+        data = reqfile.read().splitlines()
+    return data
+
 setup(
     name='vxmigration',
     version='1.0.1',
     py_modules=['import_data'],
-    install_requires=[
-        'Click',
-        'OdooRPC',
-        'Py',
-        'simplejson',
-        'psycopg2',
-        'click-log',
-    ],
+    install_requires=read_requirements(),
     entry_points='''
         [console_scripts]
         vxmigration=import_data:main
