6fa30cc580f81ffd127e68bc1b156de4d46a9eb3
Nhomar Hernandez <nhomar@vauxoo.com>
2018-01-21 14:14:45 -0600

    [REM] this module was not asked for.

--- product_cost_usd/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Vauxoo
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import product
-from . import pricelist
-from . import sale_order
